98100ef2ed28deec082c2ebb2f677c9b099b1e98
EwetoyeIbrahim <i.ewetoye@gmail.com>
2021-03-25 13:14:21 +0100

    added icon

--- __manifest__.py
+++ __manifest__.py
@@ -5,14 +5,15 @@
 # 2. There is already a module with name payment_paystack
 
 {
-    'name': 'Force Login To View Shop',
+    'name': 'Login to Shop',
     'category': 'eCommerce',
-    'summary': '''Users must login to visit shop page.''',
+    'summary': '''Users must login to view products''',
     'version': '1.0',
     'license': 'AGPL-3',
     'author': 'Ewetoye Ibrahim',
     'website': 'https://Ibrahim.Ewetoye.com',
     'description': "",
+    'images': ['static/description/icon.png'],
     'depends': ['website', 'website_sale'],
     'data': ["templates.xml"],
     'installable': True,

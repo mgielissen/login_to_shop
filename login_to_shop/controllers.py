23de4d5c5f9956c345ea7e6df94bad3a43589b48
EwetoyeIbrahim <i.ewetoye@gmail.com>
2021-04-13 09:15:13 +0100

    Fixed page parameter error

--- login_to_shop/controllers.py
+++ login_to_shop/controllers.py
@@ -2,9 +2,17 @@
 from odoo.http import request, route
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
+
 class ForceLogin(WebsiteSale):
-    @route()
-    def shop(self):
-        if not request.session.uid:    
+    @route([
+        '''/shop''',
+        '''/shop/page/<int:page>''',
+        '''/shop/category/<model("product.public.category"):category>''',
+        '''/shop/category/<model("product.public.category"):category>/page/<int:page>'''
+    ], type='http', auth="public", website=True)
+    def shop(self, page=0, category=None, search='', ppg=False, **post):
+        if not request.session.uid:
             return request.redirect('/web/login?redirect=/shop')
-        return super(ForceLogin, self).shop()
+        return super(ForceLogin, self).shop(
+            page=0, category=None, search='', ppg=False, **post
+        )
